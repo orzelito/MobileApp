3dfc8f2828671f55173d9d84510bbb318d7b4c4a
orzelitto <onlyhax12345@gmail.com>
2026-03-23 07:51:46 +0100

    add order model

---
+++ MobileApp/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MobileApp.Models
+{
+   public class Order 
+   {
+        public string Id { get; set; }
+		public List<Item> Items { get; set; }
+		public string Status { get; set; }
+		public string CreatedAt { get; set; }
+	}
+
+	public class Item
+	{
+	  public string Name { get; set; }
+	  public int Quantity { get; set; }
+
+	}
+}
