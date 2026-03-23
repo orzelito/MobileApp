44f8020840d958b9c6cf788bf34726f5eca7cacf
orzelitto <onlyhax12345@gmail.com>
2026-03-23 07:58:16 +0100

    add fake api service

---
+++ MobileApp/Services/ApiServices.cs
@@ -0,0 +1,29 @@
+﻿using MobileApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MobileApp.Services
+{
+	public class ApiService
+	{
+       public async Task<List<Order>> GetOrders()
+	   {
+			await Task.Delay(500);
+
+			return new List<Order>
+			{
+			  new Order
+			  {
+				Id = "1",
+				Status = "pending",
+				CreatedAt = "2026-03-23",
+				Items = new List<Item>
+				{
+				  new Item { Name = "Latte", Quantity = 2},
+				  new Item { Name = "Matcha", Quantity = 1},
+				}
+			  }
+			};
+	   }
+	}
+}
